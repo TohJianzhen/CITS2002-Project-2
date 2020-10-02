18ad5aeb837cdc232b51984eff1cf0b91eda3540
Chres <51875890+nchrestella1@users.noreply.github.com>
2020-10-02 18:28:50 +0700

    Header File

---
+++ mergetars.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <time.h>
+#include <sys/wait.h>
+
+#include  <sys/types.h>                                                         
+#include  <sys/stat.h> 
+
+//DEFINE ANY CONSTANTS
+#define TEMPLATE "/tmp/mt-XXXXXX"
+
+// 
