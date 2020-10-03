428c29ed83ad122ab4d436acb81e73ca68c794a5
Toh Jianzhen <tohjianzhen1@gmail.com>
2020-10-03 11:30:24 +0800

    Create Makefile.c

---
+++ Makefile.c
@@ -0,0 +1,20 @@
+# Makefile to build our 'Project 2' project
+PROJECT = mergetars
+HEADERS = $(PROJECT).h
+OBJ		= file_list.o globals.o mergetars.o
+
+C99 	= cc -std=c99
+CFLAGS	= -Wall -pedantic -Werror
+
+$(PROJECTS) : $(OBJ)
+	$(C99) $(CFLAGS) -o $(PROJECT) \ $(OBJ) -lm
+
+file_list.o : file_list.c $(HEADERS)
+	$(C99) $(CFLAGS) -c file_list.c
+
+globals.o : globals.c $(HEADERS)
+	$(C99) $(CFLAGS) -c globals.c
+
+mergetars.o : $(PROJECT).c $(HEADERS)
+	$(C99) $(CFLAGS) -c $(PROJECT).c
+
